327586ab0b9d4371752db644db4a36ad4a2cdac3
nguyenmanhhung280901 <nguyenmanhhung280901@gmail.com>
2021-06-18 10:13:28 +0700

    Upload lap6

---
+++ ConsoleApplication5/Cooler.cs
@@ -0,0 +1,28 @@
+namespace ConsoleApplication5
+{
+    public class Cooler
+    {
+        public Cooler(float temperature)
+        {
+            Temperature = temperature;
+        }
+        public float Temperature
+        {
+            get { return _Temperature; }
+            set { _Temperature = value; }
+        }
+
+        private float _Temperature;
+        public void OnTemperatureChanged(float newTemperature)
+        {
+            if (newTemperature > Temperature)
+            {
+                System.Console.WriteLine("Cooler: On");
+            }
+            else
+            {
+                System.Console.WriteLine("Cooler: Off");
+            }
+        }
+    }
+}
---
+++ Delegate_and_subscribe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Delegate_and_subscribe
+{
+    internal class Program
+    {
+        delegate void Increment(ref int X);
+         static void Add2(ref int x)
+        {
+            x += 2;
+        }
+         static void Add3(ref int x)
+        {
+            x += 3;
+        }
+        public static void Main(string[] args)
+        {
+            Increment functionDelegate = Add2;
+            functionDelegate += Add3;
+            functionDelegate += Add2;
+            int x = 5;
+            functionDelegate(ref x);
+            Console.ReadLine();
+        }
+    }
+}
